c81bee1d9274650b2ad2df6b0e969b86ab02aec4
Emil <emilsten@kth.se>
2019-08-27 14:29:58 +0200

    rename

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv')
 module.exports = {
   throwIfMissing (required = [], objWithVariables) {
     // Double check that required environment variables are set
-    for (const dotEnvDotInKey of required) {
-      if (objWithVariables[dotEnvDotInKey] === undefined) {
-        const msg = `Missing required environment variable: ${dotEnvDotInKey}`
+    for (const key of required) {
+      if (objWithVariables[key] === undefined) {
+        const msg = `Missing required environment variable: ${key}`
         throw new Error(msg)
       }
     }
