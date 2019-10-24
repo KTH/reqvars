3979f44af4d4e10564f989572e84a4b6ca7f5c0b
Carlos Saito <carlos@saito.style>
2019-10-24 14:58:55 +0200

    Command for "findVariables"

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const globby = require('globby')
+const yargs = require('yargs')
+const { findVariables } = require('../index')
+
+const argv = yargs
+  .usage('$0 [files]', 'List environmental variables of files', yargs => {
+    yargs.positional('files', {
+      describe: 'Files to look at.',
+
+      default: '*/**.js'
+    })
+  })
+  .help('h')
+  .argv
+
+const files = globby.sync(argv.files, { gitignore: true })
+console.log(findVariables(files).join('\n'))
