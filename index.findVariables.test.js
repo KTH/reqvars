41c2ac650f13baf9d363d6e6a9de413e64620427
Carlos Saito <carlos@saito.style>
2019-10-24 15:26:30 +0200

    Omit CLI tests in Node.js < 12

--- index.findVariables.test.js
+++ index.findVariables.test.js
@@ -3,18 +3,20 @@ const path = require('path')
 
 const reqvars = require('.')
 
-test('Should return all variables in several files', t => {
-  t.deepEqual(
-    reqvars.findVariables([
-      path.resolve(__dirname, './utils/fixtures/test0001.js'),
-      path.resolve(__dirname, './utils/fixtures/test0002.js'),
-      path.resolve(__dirname, './utils/fixtures/test0003.js')
-    ]),
-    [
-      'VARIABLE_ONE',
-      'VARIABLE_TWO',
-      'VARIABLE_THREE',
-      'VARIABLE_FOUR'
-    ]
-  )
-})
+if (!process.env.SKIP_CLI) {
+  test('Should return all variables in several files', t => {
+    t.deepEqual(
+      reqvars.findVariables([
+        path.resolve(__dirname, './utils/fixtures/test0001.js'),
+        path.resolve(__dirname, './utils/fixtures/test0002.js'),
+        path.resolve(__dirname, './utils/fixtures/test0003.js')
+      ]),
+      [
+        'VARIABLE_ONE',
+        'VARIABLE_TWO',
+        'VARIABLE_THREE',
+        'VARIABLE_FOUR'
+      ]
+    )
+  })
+}
--- utils/index.test.js
+++ utils/index.test.js
@@ -2,30 +2,32 @@ const test = require('ava')
 const utils = require('./index')
 const path = require('path')
 
-test('List no variables in an empty file', t => {
-  t.deepEqual(
-    utils.listVariables(path.resolve(__dirname, './fixtures/test0001.js')),
-    []
-  )
-})
+if (!process.env.SKIP_CLI) {
+  test('List no variables in an empty file', t => {
+    t.deepEqual(
+      utils.listVariables(path.resolve(__dirname, './fixtures/test0001.js')),
+      []
+    )
+  })
 
-test('List the only variable in a file', t => {
-  t.deepEqual(
-    utils.listVariables(path.resolve(__dirname, './fixtures/test0002.js')),
-    ['VARIABLE_ONE']
-  )
-})
+  test('List the only variable in a file', t => {
+    t.deepEqual(
+      utils.listVariables(path.resolve(__dirname, './fixtures/test0002.js')),
+      ['VARIABLE_ONE']
+    )
+  })
 
-test('List the 4 variables in a file', t => {
-  t.deepEqual(
-    utils.listVariables(path.resolve(__dirname, './fixtures/test0003.js')),
-    ['VARIABLE_ONE', 'VARIABLE_TWO', 'VARIABLE_THREE', 'VARIABLE_FOUR']
-  )
-})
+  test('List the 4 variables in a file', t => {
+    t.deepEqual(
+      utils.listVariables(path.resolve(__dirname, './fixtures/test0003.js')),
+      ['VARIABLE_ONE', 'VARIABLE_TWO', 'VARIABLE_THREE', 'VARIABLE_FOUR']
+    )
+  })
 
-test('List the 7 variables in a file', t => {
-  t.deepEqual(
-    utils.listVariables(path.resolve(__dirname, './fixtures/test0004.js')),
-    ['NODE_ENV', 'LOG_LEVEL', 'PROXY_PATH', 'CANVAS_API_URL', 'UG_URL', 'KOPPS_API_URL', 'PORT']
-  )
-})
+  test('List the 7 variables in a file', t => {
+    t.deepEqual(
+      utils.listVariables(path.resolve(__dirname, './fixtures/test0004.js')),
+      ['NODE_ENV', 'LOG_LEVEL', 'PROXY_PATH', 'CANVAS_API_URL', 'UG_URL', 'KOPPS_API_URL', 'PORT']
+    )
+  })
+}
